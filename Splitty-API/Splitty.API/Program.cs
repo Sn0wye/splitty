4d0d91f20abc66c9d6fbd0593c5d4def2966fe46
Gabriel Trzimajewski <95551770+Sn0wye@users.noreply.github.com>
2026-08-17 23:45:35 -0300

    feat: add scalar

--- Splitty-API/Splitty.API/Program.cs
+++ Splitty-API/Splitty.API/Program.cs
@@ -15,6 +15,7 @@ using Splitty.Repository.Interfaces;
 using Splitty.Seeder;
 using Splitty.Service;
 using Splitty.Service.Interfaces;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Logging.AddConsole();
@@ -100,6 +101,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 // Middleware
